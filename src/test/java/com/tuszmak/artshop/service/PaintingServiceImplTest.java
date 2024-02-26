7999b032756b41d563c32a8fb6b91984d75b80d2
Tuszmak <daniel.pinter02@gmail.com>
2024-02-26 10:45:59 +0100

    feat: add tests for modifyPainting

--- src/test/java/com/tuszmak/artshop/service/PaintingServiceImplTest.java
+++ src/test/java/com/tuszmak/artshop/service/PaintingServiceImplTest.java
@@ -7,6 +7,9 @@ import com.tuszmak.artshop.model.Painting;
 import com.tuszmak.artshop.repository.ArtistRepository;
 import com.tuszmak.artshop.repository.PaintingRepository;
 import com.tuszmak.artshop.utils.ToDTOUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -23,7 +26,7 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class PaintingServiceImplTest {
     @Mock
-    private PaintingRepository repository;
+    private PaintingRepository paintingRepository;
     @Mock
     private ArtistRepository artistRepository;
 
@@ -33,11 +36,11 @@ class PaintingServiceImplTest {
     @Test
     void createPainting() {
         NewPainting newPainting = new NewPainting("foo", "painting", 2002, 125.5, "Foo", 64.0, 46.0);
-        when(repository.save(any(Painting.class))).thenReturn(new Painting());
+        when(paintingRepository.save(any(Painting.class))).thenReturn(new Painting());
         Painting actual = paintingService.createPainting(newPainting);
         Painting expected = new Painting();
-        assert expected.equals(actual);
-        verify(repository, times(1)).save(any(Painting.class));
+        Assertions.assertEquals(expected, actual);
+        verify(paintingRepository, times(1)).save(any(Painting.class));
         verify(artistRepository, times(1)).getArtistByName(any(String.class));
     }
 
@@ -54,7 +57,7 @@ class PaintingServiceImplTest {
                 120.0,
                 90.0
         );
-        when(repository.getPaintingsByPublicId(any(Long.class)))
+        when(paintingRepository.getPaintingsByPublicId(any(Long.class)))
                 .thenReturn(Optional.of(painting));
         PaintingDTO expected = ToDTOUtil.toPaintingDTO(painting);
         PaintingDTO actual = paintingService.getPaintingById(1L);
@@ -64,9 +67,93 @@ class PaintingServiceImplTest {
     @Test
     void should_return_none() {
 
-        when(repository.getPaintingsByPublicId(any(Long.class)))
+        when(paintingRepository.getPaintingsByPublicId(any(Long.class)))
                 .thenReturn(Optional.empty());
         PaintingDTO actual = paintingService.getPaintingById(1L);
         assert null == actual;
+    }
+
+    @Nested
+    class modifyPaintingTests {
+        private Painting painting;
+        private NewPainting newPainting;
+        private Painting returnedPainting;
+
+        @BeforeEach
+        void setUp() {
+            painting = new Painting(
+                    UUID.randomUUID(), // Generate a random UUID
+                    1L, // Set the publicId
+                    "Starry Night",
+                    new Artist(),
+                    1889,
+                    1000000.0,
+                    "A beautiful night scene",
+                    120.0,
+                    90.0
+            );
+
+        }
+
+        @Test
+        void should_return_new_painting() {
+            newPainting = new NewPainting(
+                    "Mona Lisa",
+                    "Leonardo da Vinci",
+                    1503,
+                    2560.00,
+                    "A renowned portrait of Lisa del Giocondo",
+                    77.0,
+                    53.0
+            );
+            returnedPainting = new Painting(
+                    UUID.randomUUID(),
+                    1L,
+                    "Mona Lisa",
+                    new Artist(),
+                    1503,
+                    2560.00,
+                    "A renowned portrait of Lisa del Giocondo",
+                    77.0,
+                    53.0
+            );
+            when(paintingRepository.save(any(Painting.class))).thenReturn(returnedPainting);
+            when(paintingRepository.findById(1L)).thenReturn(Optional.ofNullable(painting));
+            when(artistRepository.getArtistByName(anyString())).thenReturn(Optional.of(new Artist()));
+            PaintingDTO actual = paintingService.modifyPaintingById(1L, newPainting);
+            PaintingDTO expected = ToDTOUtil.toPaintingDTO(returnedPainting);
+            Assertions.assertEquals(expected, actual);
+        }
+
+        @Test
+        void should_modify_only_the_name() {
+            newPainting = new NewPainting(
+                    "Leonardo da Vinci",
+                    "Mona Lisa",
+                    1503,
+                    2560.00,
+                    "A renowned portrait of Lisa del Giocondo",
+                    77.0,
+                    53.0
+            );
+            returnedPainting = new Painting(
+                    UUID.randomUUID(), // Generate a random UUID
+                    1L, // Set the publicId
+                    "Mona Lisa",
+                    new Artist(),
+                    1889,
+                    1000000.0,
+                    "A beautiful night scene",
+                    120.0,
+                    90.0
+            );
+            when(paintingRepository.save(any(Painting.class))).thenReturn(returnedPainting);
+            when(paintingRepository.findById(1L)).thenReturn(Optional.ofNullable(painting));
+            when(artistRepository.getArtistByName(anyString())).thenReturn(Optional.of(new Artist()));
+            PaintingDTO actual = paintingService.modifyPaintingById(1L, newPainting);
+            PaintingDTO expected = ToDTOUtil.toPaintingDTO(returnedPainting);
+            Assertions.assertEquals(expected, actual);
+            Assertions.assertEquals(actual.name(), "Mona Lisa");
+        }
     }
 }
