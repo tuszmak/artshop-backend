4e03c87a06bef6ac0d26db82faf3ff6671998954
Tuszmak <daniel.pinter02@gmail.com>
2024-02-26 10:12:56 +0100

    feat: modify interface

--- src/main/java/com/tuszmak/artshop/service/IPaintingService.java
+++ src/main/java/com/tuszmak/artshop/service/IPaintingService.java
@@ -4,8 +4,12 @@ import com.tuszmak.artshop.dto.NewPainting;
 import com.tuszmak.artshop.dto.PaintingDTO;
 import com.tuszmak.artshop.model.Painting;
 
+import java.util.NoSuchElementException;
+
 public interface IPaintingService {
     Painting createPainting(NewPainting newPainting);
 
     PaintingDTO getPaintingById(long paintingId);
+
+    PaintingDTO modifyPaintingById(long paintingId, NewPainting painting) throws NoSuchElementException;
 }
