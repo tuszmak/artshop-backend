b31ef8ea8bc5d69390986fd19234c64cce824a60
Tuszmak <daniel.pinter02@gmail.com>
2024-02-26 11:16:47 +0100

    fix: version previously ammended files

---
+++ src/main/java/com/tuszmak/artshop/ArtshopApplication.java
@@ -0,0 +1,13 @@
+package com.tuszmak.artshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class ArtshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuszmak/artshop/ArtshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuszmak.artshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
