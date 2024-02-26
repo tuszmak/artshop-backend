ad966ab8b63d8ae09845735357f93b173474d6e0
Tuszmak <daniel.pinter02@gmail.com>
2024-02-26 10:46:30 +0100

    style: switch variable name order to match Painting

--- src/main/java/com/tuszmak/artshop/dto/NewPainting.java
+++ src/main/java/com/tuszmak/artshop/dto/NewPainting.java
@@ -1,6 +1,6 @@
 package com.tuszmak.artshop.dto;
 
-public record NewPainting(String artistName, String name, int createdYear, double price, String description,
+public record NewPainting(String name, String artistName, int createdYear, double price, String description,
                           double width,
                           double height) {
 }
